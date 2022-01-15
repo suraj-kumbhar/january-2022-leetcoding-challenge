af8baa06b4c1cc445055fed90eb1567def1e51f3
Suraj Kumbhar <surajkannankumbhar@gmail.com>
2022-01-15 22:19:44 +0530

    add solution to week-3 day-15
    - add solution to 1345: Jump Game IV

---
+++ week-3/day-15-jump-game-iv.cpp
@@ -0,0 +1,74 @@
+// 1345. Jump Game IV
+// Hard
+
+// 1468
+
+// 63
+
+// Add to List
+
+// Share
+// Given an array of integers arr, you are initially positioned at the first index of the array.
+
+// In one step you can jump from index i to index:
+
+// i + 1 where: i + 1 < arr.length.
+// i - 1 where: i - 1 >= 0.
+// j where: arr[i] == arr[j] and i != j.
+// Return the minimum number of steps to reach the last index of the array.
+
+// Notice that you can not jump outside of the array at any time.
+
+ 
+
+// Example 1:
+
+// Input: arr = [100,-23,-23,404,100,23,23,23,3,404]
+// Output: 3
+// Explanation: You need three jumps from index 0 --> 4 --> 3 --> 9. Note that index 9 is the last index of the array.
+// Example 2:
+
+// Input: arr = [7]
+// Output: 0
+// Explanation: Start index is the last index. You do not need to jump.
+// Example 3:
+
+// Input: arr = [7,6,9,6,9,6,9,7]
+// Output: 1
+// Explanation: You can jump directly from index 0 to index 7 which is last index of the array.
+ 
+
+// Constraints:
+
+// 1 <= arr.length <= 5 * 104
+// -108 <= arr[i] <= 108
+
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int, vector<int>> indicesOfValue;
+        for (int i = 0; i < n; i++)
+            indicesOfValue[arr[i]].push_back(i);
+        vector<bool> visited(n); visited[0] = true;
+        queue<int> q; q.push(0);
+        int step = 0;
+        while (!q.empty()) {
+            for (int size = q.size(); size > 0; --size) {
+                int i = q.front(); q.pop();
+                if (i == n - 1) return step; // Reached to last index
+                vector<int>& next = indicesOfValue[arr[i]];
+                next.push_back(i - 1); next.push_back(i + 1);
+                for (int j : next) {
+                    if (j >= 0 && j < n && !visited[j]) {
+                        visited[j] = true;
+                        q.push(j);
+                    }
+                }
+                next.clear(); // avoid later lookup indicesOfValue arr[i]
+            }
+            step++;
+        }
+        return 0;
+    }
+};
